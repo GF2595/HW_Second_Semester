f63a5d3b8be2c357daff15309f9a08a46b733bfc
GF2595 <gf2595@gmail.com>
2015-04-02 23:16:30 +0300

    Task 3 unit-tests initial commit

---
+++ 13.02/13.02(3)/13.02(3).Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _13._02_3_.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
